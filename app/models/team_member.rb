350a18faea13b4f67472b7f3f66d6f434b022ad3
Garett Arrowood <garettarrowood@gmail.com>
2017-04-21 22:51:23 -0400

    Remove selfs and private specification

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -2,15 +2,13 @@ class TeamMember < ActiveRecord::Base
   has_many :team_member_photos
 
   def get_pic(year)
-    pic = self.team_member_photos.find_by(year: year)
-    pic = self.assign_pic(year) if pic.nil?
+    pic = team_member_photos.find_by(year: year)
+    pic = assign_pic(year) if pic.nil?
     pic.url
   end
 
-private
-
   def assign_pic(year)
     photo_name = name.split(' ').join('')
-    self.team_member_photos.create(year: year, url: "#{year}_bio_pics/#{photo_name}.jpg")
+    team_member_photos.create(year: year, url: "#{year}_bio_pics/#{photo_name}.jpg")
   end
 end
