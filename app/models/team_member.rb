12fac8f1488c26b5792b2ca3067f174d4ba8ce2c
Garett Arrowood <garettarrowood@gmail.com>
2017-04-21 22:46:09 -0400

    Add self for heroku

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -2,8 +2,8 @@ class TeamMember < ActiveRecord::Base
   has_many :team_member_photos
 
   def get_pic(year)
-    pic = team_member_photos.find_by(year: year)
-    pic = assign_pic(year) if pic.nil?
+    pic = self.team_member_photos.find_by(year: year)
+    pic = self.assign_pic(year) if pic.nil?
     pic.url
   end
 
@@ -11,6 +11,6 @@ private
 
   def assign_pic(year)
     photo_name = name.split(' ').join('')
-    team_member_photos.create(year: year, url: "#{year}_bio_pics/#{photo_name}.jpg")
+    self.team_member_photos.create(year: year, url: "#{year}_bio_pics/#{photo_name}.jpg")
   end
 end
