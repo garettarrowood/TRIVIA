a43bae4345285321211590ae257a3015ae1e9ba1
Garett Arrowood <garettarrowood@gmail.com>
2017-04-23 14:42:06 -0400

    Move team history to ResultsController

---
+++ app/controllers/results_controller.rb
@@ -0,0 +1,8 @@
+require "standings_scraper"
+
+class ResultsController < ApplicationController
+  before_action :set_standing
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get '/team-2016-members', to: "contests#team_2016_members"
   get '/team-2017-members', to: "contests#team_2017_members"
   get '/team-headquarters', to: "contests#team_headquarters"
-  get '/team-history', to: "contests#team_history"
   get '/team-stats', to: "contests#team_stats"
   get '/gallery/:year', to: "contests#gallery"
+
+  resources :results, only: [:index]
 end
